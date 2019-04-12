1d906457e46d5cabf7eb561927ca325b4b56b3a2
liuxin <lx_0308@163.com>
2019-04-12 13:47:34 +0800

    工具包

--- ledger/celery_tasks/config.py
+++ ledger/celery_tasks/config.py
@@ -6,12 +6,12 @@ broker_url = settings.BROKER_URL
 result_backend = settings.RESULT_BACKEND
 
 imports = [
-    'celery_tasks.phb.tasks'
+    'celery_tasks.hpb.tasks'
 ]
 
 beat_schedule = {
     "tasks": {
-        "task": "celery_tasks.phb.tasks.post_hpb",
+        "task": "celery_tasks.hpb.tasks.post_hpb",
         "schedule":crontab(minute=0,hour="*")
         # "schedule": crontab(minute="*/1")
     }
